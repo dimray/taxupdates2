91fa643283b32e814cb85587a0c711b762f32811
happyw3bsite <158301159+happyw3bsite@users.noreply.github.com>
2025-11-22 08:13:12 +0000

    commit

--- views/Endpoints/IndividualLosses/loss-claim-index.php
+++ views/Endpoints/IndividualLosses/loss-claim-index.php
@@ -1,48 +1,51 @@
 <?php if ($type_of_business === "property"): ?>
 
-    <h2>Property Business Losses</h2>
+<h2>Property Business Losses</h2>
 
 
 
-    <p>Losses from a property business are automatically carried forward and set against future profits from the same
-        business, no claim is needed.</p>
+<p>Losses from a property business are automatically carried forward and set against future profits from the same
+    business, no claim is needed.</p>
 
 
 
-    <p> In some circumstances, property losses can be set against general income. A
-        standard residential property business is unlikely to be able to set losses against general income, HMRC's guidance
-        can be found <a target="_hmrc"
-            href="https://www.gov.uk/hmrc-internal-manuals/property-income-manual/pim4205">here</a>.</p>
+<p> In some circumstances, property losses can be set against general income. A
+    standard residential property business is unlikely to be able to set losses against general income, HMRC's guidance
+    can be found <a target="_hmrc"
+        href="https://www.gov.uk/hmrc-internal-manuals/property-income-manual/pim4205">here</a>.</p>
 
 
 
-    <p>If your loss meets the conditions to be set against general income, and you do not want to carry it forward, you can
-        create a loss claim to carry it sideways. Claims cannot be made
-        until after the end of the tax year of the loss.</p>
+<p>If your loss meets the conditions to be set against general income, and you do not want to carry it forward, you can
+    create a loss claim to carry it sideways. Claims cannot be made
+    until after the end of the tax year of the loss.</p>
 
 
 
 <?php else: ?>
 
-    <h2>Trading Losses</h2>
+<h2>Trading Losses</h2>
 
-    <p>Losses from self-employment can be set against other income in either the year of loss or the
-        previous year, or they can be carried forward to set against future self-employment profits. In some circumstances
-        losses can also be
-        set against capital gains. HMRC's guidance can be found <a target="_hmrc"
-            href="https://www.gov.uk/hmrc-internal-manuals/business-income-manual/bim85000">here</a>.
-    </p>
+<p>Losses from self-employment can be set against other income in either the year of loss or the
+    previous year, or they can be carried forward to set against future self-employment profits. In some circumstances
+    losses can also be
+    set against capital gains. HMRC's guidance can be found <a target="_hmrc"
+        href="https://www.gov.uk/hmrc-internal-manuals/business-income-manual/bim85000">here</a>.
+</p>
 
 
-    <p>If this business made a trading loss in the tax year, create a loss claim to tell HMRC
-        how you will use it. Claims cannot be made
-        until after the end of the tax year of the loss.</p>
+<p>If this business made a trading loss in the tax year, create a loss claim to tell HMRC
+    how you will use it. Claims cannot be made
+    until after the end of the tax year of the loss.</p>
 
 <?php endif; ?>
 
+<hr>
+
 <p><a href="/individual-losses/create-loss-claim">Create A Loss Claim For This Business</a></p>
 
-<p><a class="hmrc-connection" href="/business-details/list-all-businesses">Create A Loss Claim For A Different
+<p><a class="hmrc-connection" href="/business-details/list-all-businesses?year-end=true">Create A Loss Claim For A
+        Different
         Business</a></p>
 
 <p><a class="hmrc-connection" href="/individual-losses/list-loss-claims">View Loss Claims</a></p>
